919a45c5edd154ad322616a6a49fec9a3ea84855
Rick Phillips <ruuk@kodi.tv>
2017-12-09 00:05:59 -0800

    2nd commit

--- install.speech.sh
+++ install.speech.sh
@@ -1,8 +1,9 @@
 apt update
 apt -y install python2 espeak termux-api
 
+cd ~
 wget http://ruuk.github.io/service.xbmc.tts/downloads/speech.server.zip
 unzip speech.server.zip
 
-echo "python2 speech.server/server.py &" > speechserver
+echo "python2 ~/speech.server/server.py &" > speechserver
 chmod +x speechserver
