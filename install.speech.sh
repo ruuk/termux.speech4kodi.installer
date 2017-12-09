27750a94740a14a4fe4e8150ebaafcbc420252b0
Rick Phillips <ruuk@kodi.tv>
2017-12-09 14:43:54 -0800

    Install startup script to path

--- install.speech.sh
+++ install.speech.sh
@@ -4,6 +4,7 @@ apt -y install python2 espeak termux-api
 cd ~
 wget http://ruuk.github.io/service.xbmc.tts/downloads/speech.server.zip
 unzip speech.server.zip
+rm -speech.server.zip
 
-echo "python2 ~/speech.server/server.py &" > speechserver
-chmod +x speechserver
+echo "python2 ~/speech.server/server.py &" > $PREFIX/bin/speechserver
+chmod +x $PREFIX/bin/speechserver
